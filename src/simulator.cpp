99be1d04d10d1297fcc13fb3bcd9029e3916385f
ToruNiina <niina.toru.68u@gmail.com>
2016-08-08 13:18:23 +0900

    use ordered and typedef Real

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -9,17 +9,18 @@
 
 #include <omp.h>
 
+typedef float Real;
 constexpr static std::size_t width  = 72;
 constexpr static std::size_t height = 72;
-constexpr static float kB = 1.;
-constexpr static float T  = 1.;
-constexpr static std::array<float, 5> prob{{1.,
+constexpr static Real kB = 1.;
+constexpr static Real T  = 1.;
+constexpr static std::array<Real, 5> prob{{1.,
     std::exp(1.f/(-kB * T)), std::exp(2.f/(-kB * T)),
     std::exp(3.f/(-kB * T)), std::exp(4.f/(-kB * T))}};
 
 inline bool step(const bool c,
                  const bool n, const bool e, const bool s, const bool w,
-                 const float rnd)
+                 const Real rnd)
 {
     short dE = 0;
     if(c != n) --dE; else ++dE;
@@ -30,7 +31,7 @@ inline bool step(const bool c,
 }
 
 inline void step(std::array<std::array<bool, width>, height>& space,
-                 const std::size_t i, const std::size_t j, const float rnd)
+                 const std::size_t i, const std::size_t j, const Real rnd)
 {
     space[i][j] = step(space[i][j],
        space[i][(j+1 < width) ? j+1 : j+1-width],
@@ -41,6 +42,24 @@ inline void step(std::array<std::array<bool, width>, height>& space,
     return;
 }
 
+inline std::array<Real, width> genrnd(std::mt19937& mt,
+                                      std::uniform_real_distribution<Real>& uni)
+{
+    std::array<Real, width> retval;
+    for(auto iter = retval.begin(); iter < retval.end(); ++iter)
+        *iter = uni(mt);
+    return retval;
+}
+
+inline std::array<bool, width> initline(std::mt19937& mt,
+        std::bernoulli_distribution& bn)
+{
+    std::array<bool, width> retval;
+    for(auto iter = retval.begin(); iter < retval.end(); ++iter)
+        *iter = bn(mt);
+    return retval;
+}
+
 int main()
 {
     const std::chrono::system_clock::time_point start =
@@ -49,14 +68,18 @@ int main()
     std::array<std::array<bool, width>, height> space{};
     std::mt19937 mt(10);
     std::bernoulli_distribution bn(0.5);
-    std::uniform_real_distribution<float> uni(0., 1.);
+    std::uniform_real_distribution<Real> uni(0., 1.);
 
-#pragma omp parallel for
-    for(auto outer = space.begin(); outer < space.end(); ++outer)
-        for(auto iter = outer->begin(); iter != outer->end(); ++iter)
-            *iter = bn(mt);
+#pragma omp parallel for ordered
+    for(auto iter = space.begin(); iter < space.end(); ++iter)
+    {
+#pragma omp ordered
+        {
+            *iter = initline(mt, bn);
+        }//ordered
+    }
 
-    std::array<std::array<float, width>, height> random{};
+    std::array<std::array<Real, width>, height> random{};
     std::size_t t = 0;
 
     const std::chrono::system_clock::time_point start_time =
@@ -67,8 +90,12 @@ int main()
 {
 #pragma omp for ordered
         for(auto outer = random.begin(); outer < random.end(); ++outer)
-            for(auto iter = outer->begin(); iter < outer->end(); ++iter)
-                *iter = uni(mt);
+        {
+#pragma omp ordered
+            {
+            *outer = genrnd(mt, uni);
+            }//ordered
+        }
 
 #pragma omp for
         for(std::size_t i=0; i<height; ++i)
