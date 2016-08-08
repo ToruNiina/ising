0f19a1cb78b0ebe26fe20320d546ce8eeb6423cb
ToruNiina <niina.toru.68u@gmail.com>
2016-08-08 12:48:11 +0900

    generate random number at first

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -16,26 +16,25 @@ constexpr static std::array<double, 5> prob{{1.,
 
 inline bool step(const bool c,
                  const bool n, const bool e, const bool s, const bool w,
-                 std::mt19937& mt, std::uniform_real_distribution<double>& uni)
+                 const double rnd)
 {
-    int dE = 0;
+    short dE = 0;
     if(c != n) --dE; else ++dE;
     if(c != e) --dE; else ++dE;
     if(c != s) --dE; else ++dE;
     if(c != w) --dE; else ++dE;
-    return (dE <= 0) ? (!c) : ((uni(mt) < prob[dE]) != c);
+    return (dE <= 0) ? (!c) : ((rnd < prob[dE]) != c);
 }
 
 inline void step(std::array<std::array<bool, width>, height>& space,
-                 std::size_t i, std::size_t j,
-                 std::mt19937& mt, std::uniform_real_distribution<double>& uni)
+                 const std::size_t i, const std::size_t j, const double rnd)
 {
     space[i][j] = step(space[i][j],
        space[i][(j+1 < width) ? j+1 : j+1-width],
        space[(i+1 < height) ? i+1 : i+1-height][j],
        space[i][(j-1 >= 0) ? j-1 : j-1+width],
        space[(i-1 >= 0) ? i-1 : i-1+height][j],
-       mt, uni);
+       rnd);
     return;
 }
 
@@ -48,34 +47,34 @@ int main()
     std::mt19937 mt(10);
     std::bernoulli_distribution bn(0.5);
     std::uniform_real_distribution<double> uni(0., 1.);
+
     for(auto outer = space.begin(); outer != space.end(); ++outer)
         for(auto iter = outer->begin(); iter != outer->end(); ++iter)
-        {
             *iter = bn(mt);
-        }
 
-//     std::array<std::array<double, width>, height> random{};
+    std::array<std::array<double, width>, height> random{};
     std::size_t t = 0;
 
     const std::chrono::system_clock::time_point start_time =
         std::chrono::system_clock::now();
     while(t < 100)
     {
-//         for(auto outer = random.begin(); outer != random.end(); ++outer)
-//             for(auto iter = outer->begin(); iter != outer->end(); ++iter)
-//                 *iter = uni(mt);
-
 #pragma omp parallel
 {
+#pragma omp for ordered
+        for(auto outer = random.begin(); outer < random.end(); ++outer)
+            for(auto iter = outer->begin(); iter < outer->end(); ++iter)
+                *iter = uni(mt);
+
 #pragma omp for
         for(std::size_t i=0; i<height; ++i)
         {
             if(i % 2 == 0)
                 for(std::size_t j=0; j<width; j+=2)
-                    step(space, i, j, mt, uni);
+                    step(space, i, j, random[i][j]);
             else
                 for(std::size_t j=1; j<width; j+=2)
-                    step(space, i, j, mt, uni);
+                    step(space, i, j, random[i][j]);
         }
 
 #pragma omp for
@@ -83,10 +82,10 @@ int main()
         {
             if(i % 2 == 0)
                 for(std::size_t j=1; j<width; j+=2)
-                    step(space, i, j, mt, uni);
+                    step(space, i, j, random[i][j]);
             else
                 for(std::size_t j=0; j<width; j+=2)
-                    step(space, i, j, mt, uni);
+                    step(space, i, j, random[i][j]);
         }
 
 }//parallel
